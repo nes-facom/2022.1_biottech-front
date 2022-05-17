5e356bfddf279560c26b9f1f31d9b01e06364edf
Marialuisabcs <maria.lbcs@icloud.com>
2022-05-17 17:57:37 -0400

    feat: adds topbar buttons and 'change password' modal

--- src/AppTopbar.vue
+++ src/AppTopbar.vue
@@ -2,7 +2,7 @@
 	<div class="layout-topbar">
 		<router-link to="/" class="layout-topbar-logo">
 			<img alt="Logo" :src="topbarImage()" />
-			<span>SAKAI</span>
+			<span>Biot Tech</span>
 		</router-link>
 		<button class="p-link layout-menu-button layout-topbar-button" @click="onMenuToggle">
 			<i class="pi pi-bars"></i>
@@ -15,40 +15,77 @@
 		</button>
 		<ul class="layout-topbar-menu hidden lg:flex origin-top">
 			<li>
-				<button class="p-link layout-topbar-button">
-					<i class="pi pi-calendar"></i>
-					<span>Events</span>
+				<button @click="showModal()" class="p-link layout-topbar-button">
+					<i class="pi pi-user-edit"></i>
+					<span>Alterar senha</span>
 				</button>
 			</li>
 			<li>
-				<button class="p-link layout-topbar-button">
-					<i class="pi pi-cog"></i>
-					<span>Settings</span>
-				</button>
-			</li>
-			<li>
-				<button class="p-link layout-topbar-button">
-					<i class="pi pi-user"></i>
-					<span>Profile</span>
+				<button @click="logOut()" class="p-link layout-topbar-button">
+					<i class="pi pi-sign-out"></i>
+					<span>Sair</span>
 				</button>
 			</li>
 		</ul>
+		<Dialog position="top" v-model:visible="visible" :modal="true" :breakpoints="{'960px': '50vw', '640px': '100vw'}" :style="{width: '30vw'}">
+			<template #header>
+				<h3>Alterar senha</h3>
+			</template>
+			<div class="felx justify-content-center grid p-fluid">
+				<div class="col-12">
+					<div class="mb-4">
+						<h5>Nova senha</h5>
+        		<InputText type="text" v-model="newPassword" />
+					</div>
+					<div>
+						<h5>Confirmar Senha</h5>
+						<InputText type="text" v-model="cofirmPassword" />
+					</div>
+				</div>
+      </div>
+			<template #footer>
+				<div class="flex justify-content-between">
+					<Button @click="this.visible = false" label="Cancelar" class="p-button-danger p-button-outlined"/>
+					<Button @click="changePassword()" label="Salvar" class="p-button-success"/>
+				</div>
+			</template>
+	</Dialog>
 	</div>
 </template>
 
 <script>
 export default {
-    methods: {
-        onMenuToggle(event) {
-            this.$emit('menu-toggle', event);
-        },
+    data(){
+			return{
+				visible: false,
+				newPassword: null,
+				cofirmPassword: null
+			}
+		},
+		methods: {
+		onMenuToggle(event) {
+				this.$emit('menu-toggle', event);
+		},
 		onTopbarMenuToggle(event) {
-            this.$emit('topbar-menu-toggle', event);
-        },
-		topbarImage() {
-			return this.$appState.darkTheme ? 'images/logo-white.svg' : 'images/logo-dark.svg';
-		}
+        this.$emit('topbar-menu-toggle', event);
     },
+		topbarImage() {
+			return 'images/logo-ufms.png';
+		},
+		showModal(){
+			this.visible = true
+			console.log(this.visible);
+		},
+		logOut(){
+			console.log('Log out');
+			//TODO: inserir método de logout
+		},
+		changePassword(){
+			console.log('Change password');
+			//TODO: salvar alteração de senha
+			this.visible = false;
+		},
+  },
 	computed: {
 		darkTheme() {
 			return this.$appState.darkTheme;
