7f1d918cc4c42623a9d4707c2d201f7c65641619
Marialuisabcs <maria.lbcs@icloud.com>
2022-05-20 03:35:47 -0400

    feat: changes render component for menu

--- src/router.js
+++ src/router.js
@@ -1,276 +1,276 @@
 import { h, resolveComponent } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router';
-import App from './App.vue';
+import { createRouter, createWebHashHistory } from 'vue-router'
+import App from './App.vue'
 
 const routes = [
-    {
-      path: '/',
-      name: 'app',
-      component: App,
-      children: [
-        {
-          path: '',
-          name: 'Configurações',
-          component: () => import('./pages/CrudDemo.vue'),
+  {
+    path: '/',
+    name: 'app',
+    component: App,
+    children: [
+      {
+        path: '',
+        name: 'Configurações',
+        component: () => import('./pages/CrudDemo.vue')
+      },
+      {
+        path: '/pesquisador',
+        name: 'Pesquisador',
+        component: {
+          render() {
+            return h(resolveComponent('router-view'))
+          }
         },
-        {
-          path: '/pesquisador',
-          name: 'Pesquisador',
-          component: {
-            render() {
-              return h(resolveComponent('router-view'))
-            },
+        children: [
+          {
+            path: '',
+            component: () => import('./pages/TableCrud.vue')
           },
-          children: [
-            {
-              path: '',
-              component: () => import('./pages/CrudDemo.vue'),
-            },
-            {
-              path: '/pesquisador/desativado',
-              component: () => import('./components/TableDemo.vue'),
-            },
-          ],
+          {
+            path: '/pesquisador/desativado',
+            component: () => import('./components/TableDemo.vue')
+          }
+        ]
+      },
+      {
+        path: '/pedido',
+        name: 'pedido',
+        component: {
+          render() {
+            return h(resolveComponent('router-view'))
+          }
         },
-        {
-          path: '/pedido',
-          name: 'pedido',
-          component: {
-            render() {
-              return h(resolveComponent('router-view'))
-            },
+        children: [
+          {
+            path: '',
+            component: () => import('./pages/TableCrud.vue')
           },
-          children: [
-            {
-              path: '',
-              component: () => import('./pages/CrudDemo.vue'),
-            },
-            {
-              path: '/pedido/desativado',
-              component: () => import('./components/TableDemo.vue'),
-            },
-          ],
+          {
+            path: '/pedido/desativado',
+            component: () => import('./components/TableDemo.vue')
+          }
+        ]
+      },
+      {
+        path: '/previsao',
+        name: 'Previsao',
+        component: {
+          render() {
+            return h(resolveComponent('router-view'))
+          }
         },
-        {
-          path: '/previsao',
-          name: 'Previsao',
-          component: {
-            render() {
-              return h(resolveComponent('router-view'))
-            },
+        children: [
+          {
+            path: '',
+            component: () => import('./pages/TableCrud.vue')
           },
-          children: [
-            {
-              path: '',
-              component: () => import('./pages/CrudDemo.vue'),
-            },
-            {
-              path: '/previsao/desativado',
-              component: () => import('./components/TableDemo.vue'),
-            },
-          ],
+          {
+            path: '/previsao/desativado',
+            component: () => import('./components/TableDemo.vue')
+          }
+        ]
+      },
+      {
+        path: '/saida',
+        name: 'Saída',
+        component: {
+          render() {
+            return h(resolveComponent('router-view'))
+          }
         },
-        {
-          path: '/saida',
-          name: 'Saída',
-          component: {
-            render() {
-              return h(resolveComponent('router-view'))
-            },
+        children: [
+          {
+            path: '',
+            component: () => import('./pages/TableCrud.vue')
           },
-          children: [
-            {
-              path: '',
-              component: () => import('./pages/CrudDemo.vue'),
-            },
-            {
-              path: '/saida/desativado',
-              component: () => import('./components/TableDemo.vue'),
-            },
-          ],
+          {
+            path: '/saida/desativado',
+            component: () => import('./components/TableDemo.vue')
+          }
+        ]
+      },
+      {
+        path: '/caixa',
+        name: 'Caixa',
+        component: {
+          render() {
+            return h(resolveComponent('router-view'))
+          }
         },
-        {
-          path: '/caixa',
-          name: 'Caixa',
-          component: {
-            render() {
-              return h(resolveComponent('router-view'))
-            },
+        children: [
+          {
+            path: '',
+            component: () => import('./pages/TableCrud.vue')
           },
-          children: [
-            {
-              path: '',
-              component: () => import('./pages/CrudDemo.vue'),
-            },
-            {
-              path: '/caixa/desativado',
-              component: () => import('./components/TableDemo.vue'),
-            },
-          ],
+          {
+            path: '/caixa/desativado',
+            component: () => import('./components/TableDemo.vue')
+          }
+        ]
+      },
+      {
+        path: '/tempumi',
+        name: 'TemperaturaUmidade',
+        component: {
+          render() {
+            return h(resolveComponent('router-view'))
+          }
         },
-        {
-          path: '/tempumi',
-          name: 'TemperaturaUmidade',
-          component: {
-            render() {
-              return h(resolveComponent('router-view'))
-            },
+        children: [
+          {
+            path: '',
+            name: 'Menu temperatura e umidade',
+            component: () => import('./pages/TableCrud.vue')
           },
-          children: [
-            {
-              path: '',
-              name: 'Menu temperatura e umidade',
-              component: () => import('./pages/CrudDemo.vue'),
-            },
-            {
-              path: '/tempumi/desativado',
-              name: 'Temperatura e umidade arquivadas',
-              component: () => import('./components/TableDemo.vue'),
-            },
-          ],
+          {
+            path: '/tempumi/desativado',
+            name: 'Temperatura e umidade arquivadas',
+            component: () => import('./components/TableDemo.vue')
+          }
+        ]
+      },
+      {
+        path: '/cxmatriz',
+        name: 'Caixa Matriz',
+        component: {
+          render() {
+            return h(resolveComponent('router-view'))
+          }
         },
-        {
-          path: '/cxmatriz',
-          name: 'Caixa Matriz',
-          component: {
-            render() {
-              return h(resolveComponent('router-view'))
-            },
+        children: [
+          {
+            path: '',
+            name: 'Menu caixa matriz',
+            component: () => import('./pages/TableCrud.vue')
           },
-          children: [
-            {
-              path: '',
-              name: 'Menu caixa matriz',
-              component: () => import('./pages/CrudDemo.vue'),
-            },
-            {
-              path: '/cxmatriz/desativado',
-              name: 'Caixas matrizes arquivadas',
-              component: () => import('./components/TableDemo.vue'),
-            },
-          ],
+          {
+            path: '/cxmatriz/desativado',
+            name: 'Caixas matrizes arquivadas',
+            component: () => import('./components/TableDemo.vue')
+          }
+        ]
+      },
+      {
+        path: '/parto',
+        name: 'Parto',
+        component: {
+          render() {
+            return h(resolveComponent('router-view'))
+          }
         },
-        {
-          path: '/parto',
-          name: 'Parto',
-          component: {
-            render() {
-              return h(resolveComponent('router-view'))
-            },
+        children: [
+          {
+            path: '',
+            name: 'Menu parto',
+            component: () => import('./pages/TableCrud.vue')
           },
-          children: [
-            {
-              path: '',
-              name: 'Menu parto',
-              component: () => import('./pages/CrudDemo.vue'),
-            },
-            {
-              path: '/parto/desativado',
-              name: 'Partos arquivados',
-              component: () => import('./components/TableDemo.vue'),
-            },
-          ],
+          {
+            path: '/parto/desativado',
+            name: 'Partos arquivados',
+            component: () => import('./components/TableDemo.vue')
+          }
+        ]
+      },
+      {
+        path: '/exp',
+        name: 'Experimentação',
+        component: {
+          render() {
+            return h(resolveComponent('router-view'))
+          }
         },
-        {
-          path: '/exp',
-          name: 'Experimentação',
-          component: {
-            render() {
-              return h(resolveComponent('router-view'))
-            },
+        children: [
+          {
+            path: '/exp/pedido',
+            name: 'Pedido',
+            component: () => import('./components/TableDemo.vue')
           },
-          children: [
-            {
-              path: '/exp/pedido',
-              name: 'Pedido',
-              component: () => import('./components/TableDemo.vue'),
-            },
-            {
-              path: '/exp/pesquisadores',
-              name: 'Pesquisadores',
-              component: () => import('./components/TableDemo.vue'),
-            },
-            {
-              path: '/exp/previsao',
-              name: 'Previsão',
-              component: () => import('./components/TableDemo.vue'),
-            },
-            {
-              path: '/exp/resultado',
-              name: 'Resultado',
-              component: () => import('./components/TableDemo.vue'),
-            },
-          ],
-        },
-        {
-          path: '/criacao',
-          name: 'Criação',
-          component: {
-            render() {
-              return h(resolveComponent('router-view'))
-            },
+          {
+            path: '/exp/pesquisadores',
+            name: 'Pesquisadores',
+            component: () => import('./components/TableDemo.vue')
           },
-          children: [
-            {
-              path: '/criacao/dados',
-              name: 'Entrada Dados',
-              component: () => import('./components/TableDemo.vue'),
-            },
-            {
-              path: '/criacao/saidas',
-              name: 'Saídas',
-              component: () => import('./components/TableDemo.vue'),
-            },
-            {
-              path: '/criacao/tempumi',
-              name: 'Temperatura e Umidade',
-              component: () => import('./components/TableDemo.vue'),
-            },
-          ],
-        },
-        {
-          path: '/repro',
-          name: 'Reprodução',
-          component: {
-            render() {
-              return h(resolveComponent('router-view'))
-            },
+          {
+            path: '/exp/previsao',
+            name: 'Previsão',
+            component: () => import('./components/TableDemo.vue')
           },
-          children: [
-            {
-              path: '/repro/nascdesma',
-              name: 'Nascimento e Desmame',
-              component: () => import('./components/TableDemo.vue'),
-            },
-            {
-              path: '/repro/matrizes',
-              name: 'Matrizes',
-              component: () => import('./components/TableDemo.vue'),
-            },
-            {
-              path: '/repro/progacasal',
-              name: 'Programação Acasalamento',
-              component: () => import('./components/TableDemo.vue'),
-            },
-            {
-              path: '/repro/controlerepro',
-              name: 'Controle Reprodutivo',
-              component: () => import('./components/TableDemo.vue'),
-            },
-          ],
+          {
+            path: '/exp/resultado',
+            name: 'Resultado',
+            component: () => import('./components/TableDemo.vue')
+          }
+        ]
+      },
+      {
+        path: '/criacao',
+        name: 'Criação',
+        component: {
+          render() {
+            return h(resolveComponent('router-view'))
+          }
         },
-      ],
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: () => import('./pages/Login.vue'),
-    },
-  ]
-  
+        children: [
+          {
+            path: '/criacao/dados',
+            name: 'Entrada Dados',
+            component: () => import('./components/TableDemo.vue')
+          },
+          {
+            path: '/criacao/saidas',
+            name: 'Saídas',
+            component: () => import('./components/TableDemo.vue')
+          },
+          {
+            path: '/criacao/tempumi',
+            name: 'Temperatura e Umidade',
+            component: () => import('./components/TableDemo.vue')
+          }
+        ]
+      },
+      {
+        path: '/repro',
+        name: 'Reprodução',
+        component: {
+          render() {
+            return h(resolveComponent('router-view'))
+          }
+        },
+        children: [
+          {
+            path: '/repro/nascdesma',
+            name: 'Nascimento e Desmame',
+            component: () => import('./components/TableDemo.vue')
+          },
+          {
+            path: '/repro/matrizes',
+            name: 'Matrizes',
+            component: () => import('./components/TableDemo.vue')
+          },
+          {
+            path: '/repro/progacasal',
+            name: 'Programação Acasalamento',
+            component: () => import('./components/TableDemo.vue')
+          },
+          {
+            path: '/repro/controlerepro',
+            name: 'Controle Reprodutivo',
+            component: () => import('./components/TableDemo.vue')
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('./pages/Login.vue')
+  }
+]
+
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes,
-});
-  
+  history: createWebHashHistory(),
+  routes
+})
+
 export default router
