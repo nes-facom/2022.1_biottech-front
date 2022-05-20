93e445c06094260de5fbd536f791facf31e939a6
Marialuisabcs <maria.lbcs@icloud.com>
2022-05-20 14:03:01 -0400

    feat: creates login page

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,65 +1,76 @@
 <template>
-    <div class="surface-0 flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden">
-        <div class="grid justify-content-center p-2 lg:p-0" style="min-width:80%">
-            <div class="col-12 mt-5 xl:mt-0 text-center">
-                <img :src="'layout/images/logo-' + logoColor + '.svg'" alt="Sakai logo" class="mb-5" style="width:81px; height:60px;">
+  <div
+    class="surface-300 flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden">
+    <div class="grid justify-content-center" style="min-width: 80%">
+      <Card style="width: 40em">
+        <template #header>
+          <div class="col-12 mt-5 xl:mt-2 text-center">
+            <img
+              :src="'images/logo/png/logo-text-d.png'"
+              alt="Biottech logo"
+              style="width: 170px; height: 140px" />
+          </div>
+        </template>
+        <template #title>
+          <div class="grid justify-content-center">
+            <p class="text-4xl" style="color: #2596be">Seja bem-vindo</p>
+          </div>
+        </template>
+        <template #content>
+          <div class="h-full w-full">
+            <div class="w-full md:w-10 mx-auto">
+              <label
+                for="email"
+                class="block text-900 text-xl font-medium mb-2">
+                Email
+              </label>
+              <InputText
+                id="email"
+                v-model="email"
+                type="text"
+                class="w-full mb-5"
+                placeholder="Email"
+                style="padding: 1rem" />
+
+              <label
+                for="senha"
+                class="block text-900 font-medium text-xl mb-2">
+                Senha
+              </label>
+              <Password
+                id="senha"
+                v-model="senha"
+                placeholder="senha"
+                :toggleMask="true"
+                :feedback="false"
+                class="w-full mb-3"
+                inputClass="w-full"
+                inputStyle="padding:1rem"></Password>
             </div>
-            <div class="col-12 xl:col-6" style="border-radius:56px; padding:0.3rem; background: linear-gradient(180deg, var(--primary-color), rgba(33, 150, 243, 0) 30%);">
-                <div class="h-full w-full m-0 py-7 px-4" style="border-radius:53px; background: linear-gradient(180deg, var(--surface-50) 38.9%, var(--surface-0));">
-                    <div class="text-center mb-5">
-                        <img src="layout/images/avatar.png" alt="Image" height="50" class="mb-3">
-                        <div class="text-900 text-3xl font-medium mb-3">Welcome, Isabel!</div>
-                        <span class="text-600 font-medium">Sign in to continue</span>
-                    </div>
-                
-                    <div class="w-full md:w-10 mx-auto">
-                        <label for="email1" class="block text-900 text-xl font-medium mb-2">Email</label>
-                        <InputText id="email1" v-model="email" type="text" class="w-full mb-3" placeholder="Email" style="padding:1rem;" />
-                
-                        <label for="password1" class="block text-900 font-medium text-xl mb-2">Password</label>
-                        <Password id="password1" v-model="password" placeholder="Password" :toggleMask="true" class="w-full mb-3" inputClass="w-full" inputStyle="padding:1rem"></Password>
-                
-                        <div class="flex align-items-center justify-content-between mb-5">
-                            <div class="flex align-items-center">
-                                <Checkbox id="rememberme1" v-model="checked" :binary="true" class="mr-2"></Checkbox>
-                                <label for="rememberme1">Remember me</label>
-                            </div>
-                            <a class="font-medium no-underline ml-2 text-right cursor-pointer" style="color: var(--primary-color)">Forgot password?</a>
-                        </div>
-                        <Button label="Sign In" class="w-full p-3 text-xl"></button>
-                    </div>
-                </div>
-            </div>
-        </div>
+          </div>
+        </template>
+        <template #footer>
+          <div class="grid justify-content-center mb-5">
+            <Button label="Entrar" class="p-button-info w-10 text-xl"></Button>
+          </div>
+        </template>
+      </Card>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            email: '',
-            password: '',
-            checked: false
-        }
-    },
-    computed: {
-        logoColor() {
-            if (this.$appState.darkTheme) return 'white';
-            return 'dark';
-        }
+  data() {
+    return {
+      email: '',
+      senha: ''
     }
+  },
+  computed: {
+    logoColor() {
+      return 'white'
+    }
+  }
 }
 </script>
-
-<style scoped>
-.pi-eye {
-    transform:scale(1.6);
-    margin-right: 1rem;
-}
-
-.pi-eye-slash {
-    transform:scale(1.6);
-    margin-right: 1rem;
-}
-</style>
