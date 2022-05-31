a0f6e202cd8d5726bac7806704c8f298ad010617
Marialuisabcs <maria.lbcs@icloud.com>
2022-05-31 14:15:28 -0400

    feat: creates dashboard component

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="grid">
+    <div class="col-12">
+      <div class="card">
+        <h5>Exemplo de gráfico</h5>
+        <Chart type="line" :data="basicData" :options="basicOptions" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      basicData: {
+        labels: [
+          'Janeiro',
+          'Fevereiro',
+          'Março',
+          'Abril',
+          'Maio',
+          'Junho',
+          'Julho'
+        ],
+        datasets: [
+          {
+            label: 'Primeiro Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: '#42A5F5',
+            tension: 0.4
+          },
+          {
+            label: 'Segundo Dataset',
+            data: [28, 48, 40, 19, 86, 27, 90],
+            fill: false,
+            borderColor: '#FFA726',
+            tension: 0.4
+          }
+        ]
+      },
+      basicOptions: {
+        plugins: {
+          legend: {
+            labels: {
+              color: '#495057'
+            }
+          }
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: '#495057'
+            },
+            grid: {
+              color: '#ebedef'
+            }
+          },
+          y: {
+            ticks: {
+              color: '#495057'
+            },
+            grid: {
+              color: '#ebedef'
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</script>
