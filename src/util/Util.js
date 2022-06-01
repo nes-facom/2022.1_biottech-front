9830b6b0471d0db84a8ea63fd63c6fb8f2882f6e
Marialuisabcs <maria.lbcs@icloud.com>
2022-06-01 17:19:08 -0400

    feat: add year list

--- src/util/Util.js
+++ src/util/Util.js
@@ -1,20 +1,23 @@
 class Util {
+  formatPhones(obj) {
+    const arr = Object.keys(obj).map(function (key) {
+      return obj[key]
+    })
+    let str = ''
+    arr.forEach((element) => {
+      if (str == '') {
+        str += element.telefone
+      } else {
+        str += '\n' + element.telefone
+      }
+    })
+    return str
+  }
 
-    formatPhones(obj) {
-        const arr = Object.keys(obj).map(function (key) {
-            return obj[key]
-        })
-        let str = ''
-        arr.forEach((element) => {
-            if (str == '') {
-                str += element.telefone
-            } else {
-                str += '\n' + element.telefone
-            }
-        })
-        return str
-    }
+  getListYears() {
+    const years = [2018, 2019, 2020, 2021, 2022]
+    return years
+  }
 }
 
-
-export default new Util();
+export default new Util()
