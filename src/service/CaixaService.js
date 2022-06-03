c463a5a089c9a5e3e5692334561dc7ef52e671a6
Marialuisabcs <maria.lbcs@icloud.com>
2022-06-02 20:02:20 -0400

    feat: changes columns order

--- src/service/CaixaService.js
+++ src/service/CaixaService.js
@@ -1,57 +1,62 @@
-import AuthService from "./AuthService";
-import axios from "axios";
-import { API_ENDPOINT } from "../constants";
-import Util from "../util/Util";
-
+import AuthService from './AuthService'
+import axios from 'axios'
+import { API_ENDPOINT } from '../constants'
+import Util from '../util/Util'
 
 class Caixa {
-    async #getCaixa(search, page, year) {
-        return axios.get(`${API_ENDPOINT}/caixa/getCaixas.json?page=${page}&limit=8&active=true&year=${year}&search=${search}`, this.buildAuthHeader())
-    }
+  async #getCaixa(search, page, year) {
+    return axios.get(
+      `${API_ENDPOINT}/caixa/getCaixas.json?page=${page}&limit=8&active=true&year=${year}&search=${search}`,
+      this.buildAuthHeader()
+    )
+  }
 
-    async #getCaixaInactive(search, page, year) {
-        return axios.get(`${API_ENDPOINT}/caixa/getCaixas.json?page=${page}&limit=8&active=true&year=${year}&search=${search}`, this.buildAuthHeader())
-    }
+  async #getCaixaInactive(search, page, year) {
+    return axios.get(
+      `${API_ENDPOINT}/caixa/getCaixas.json?page=${page}&limit=8&active=true&year=${year}&search=${search}`,
+      this.buildAuthHeader()
+    )
+  }
 
-    getCaixas(disable, page, search, year, onFetch) {
-        if (disable) {
-            this.#getCaixaInactive(search, page, year)
-                .then(response => onFetch(this.#formatDate(response.data)))
-        } else {
-            this.#getCaixa(search, page, year)
-                .then(response => onFetch(this.#formatDate(response.data)))
-        }
+  getCaixas(disable, page, search, year, onFetch) {
+    if (disable) {
+      this.#getCaixaInactive(search, page, year).then((response) =>
+        onFetch(this.#formatDate(response.data))
+      )
+    } else {
+      this.#getCaixa(search, page, year).then((response) =>
+        onFetch(this.#formatDate(response.data))
+      )
     }
+  }
 
-    #formatDate(data) {
-        data.caixas.map(
-            caixa => {
-                caixa.nascimento = Util.formatDateTable(caixa.nascimento)
-            }
-        )
-        return data
-    }
+  #formatDate(data) {
+    data.caixas.map((caixa) => {
+      caixa.nascimento = Util.formatDateTable(caixa.nascimento)
+    })
+    return data
+  }
 
-    getCaixaHeaders() {
-        const columns = [
-            { field: 'caixa_numero', header: 'Número da Caixa' },
-            { field: 'nascimento', header: 'Data de Nascimento' },
-            { field: 'sexo', header: 'Sexo' },
-            { field: 'num_animais', header: 'Número de animais' },
-            { field: 'qtd_saida', header: 'Quantidade Saída' },
-            { field: 'ultima_saida', header: 'Última Saída' },
-            { field: 'linhagem.nome_linhagem', header: 'Linhagem' }
-        ]
-        return columns
-    }
+  getCaixaHeaders() {
+    const columns = [
+      { field: 'caixa_numero', header: 'Número da Caixa' },
+      { field: 'linhagem.nome_linhagem', header: 'Linhagem' },
+      { field: 'nascimento', header: 'Data de Nascimento' },
+      { field: 'sexo', header: 'Sexo' },
+      { field: 'num_animais', header: 'Número de animais' },
+      { field: 'qtd_saida', header: 'Quantidade Saída' },
+      { field: 'ultima_saida', header: 'Última Saída' }
+    ]
+    return columns
+  }
 
-    buildAuthHeader() {
-        return {
-            headers: {
-                'Authorization': `Bearer ${AuthService.getJWTToken()}`
-            }
-        }
+  buildAuthHeader() {
+    return {
+      headers: {
+        Authorization: `Bearer ${AuthService.getJWTToken()}`
+      }
     }
+  }
 }
 
 export default new Caixa()
--- src/service/PedidoService.js
+++ src/service/PedidoService.js
@@ -1,49 +1,56 @@
-import AuthService from "./AuthService";
-import axios from "axios";
-import { API_ENDPOINT } from "../constants";
-import Util from "../util/Util";
-
+import AuthService from './AuthService'
+import axios from 'axios'
+import { API_ENDPOINT } from '../constants'
+import Util from '../util/Util'
 
 class Pedido {
   async #getPedido(search, page, year) {
-    return axios.get(`${API_ENDPOINT}/pedido/getPedidos.json?page=${page}&limit=8&active=true&year=${year}&search=${search}`, this.buildAuthHeader())
+    return axios.get(
+      `${API_ENDPOINT}/pedido/getPedidos.json?page=${page}&limit=8&active=true&year=${year}&search=${search}`,
+      this.buildAuthHeader()
+    )
   }
 
   async #getPedidoInactive(search, page, year) {
-    return axios.get(`${API_ENDPOINT}/pedido/getPedidos.json?page=${page}&limit=8&active=true&year=${year}&search=${search}`, this.buildAuthHeader())
+    return axios.get(
+      `${API_ENDPOINT}/pedido/getPedidos.json?page=${page}&limit=8&active=true&year=${year}&search=${search}`,
+      this.buildAuthHeader()
+    )
   }
 
   getPedidos(disable, page, search, year, onFetch) {
     if (disable) {
-      this.#getPedidoInactive(search, page, year)
-        .then(response => onFetch(this.#formatDate(response.data)))
+      this.#getPedidoInactive(search, page, year).then((response) =>
+        onFetch(this.#formatDate(response.data))
+      )
     } else {
-      this.#getPedido(search, page, year)
-        .then(response => onFetch(this.#formatDate(response.data)))
+      this.#getPedido(search, page, year).then((response) =>
+        onFetch(this.#formatDate(response.data))
+      )
     }
   }
 
   #formatDate(data) {
-    data.pedidos.map(
-      pedido => {
-        pedido.data_solicitacao = Util.formatDateTable(pedido.data_solicitacao),
-          pedido.vigencia_ceua = Util.formatDateTable(pedido.vigencia_ceua)
-      }
-    )
+    data.pedidos.map((pedido) => {
+      ;(pedido.data_solicitacao = Util.formatDateTable(
+        pedido.data_solicitacao
+      )),
+        (pedido.vigencia_ceua = Util.formatDateTable(pedido.vigencia_ceua))
+    })
     return data
   }
 
   getPedidoHeaders() {
     const columns = [
-      { field: 'processo_sei', header: 'Sei' },
       { field: 'pesquisador.nome', header: 'Pesquisador' },
       { field: 'data_solicitacao', header: 'Solicitação' },
-      { field: 'num_ceua', header: 'Ceua' },
       { field: 'vigencia_ceua', header: 'Vigência' },
       { field: 'num_aprovado', header: 'N°Aprovados' },
       { field: 'num_solicitado', header: 'N°Solicitado' },
-      { field: 'saldoCEUA', header: 'Saldo CEUA' },
-      { field: 'linhagem.nome_linhagem', header: 'Linhagem' }
+      { field: 'linhagem.nome_linhagem', header: 'Linhagem' },
+      { field: 'sexo', header: 'Sexo' },
+      { field: 'idade', header: 'Idade' },
+      { field: 'peso', header: 'Peso' }
     ]
     return columns
   }
@@ -51,7 +58,7 @@ class Pedido {
   buildAuthHeader() {
     return {
       headers: {
-        'Authorization': `Bearer ${AuthService.getJWTToken()}`
+        Authorization: `Bearer ${AuthService.getJWTToken()}`
       }
     }
   }
