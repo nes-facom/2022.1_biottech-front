508435d90cee6035dc01a5a84d02c9f6b1b2280b
Marialuisabcs <maria.lbcs@icloud.com>
2022-06-06 17:17:01 -0400

    feat: creates usuarioModal

---
+++ src/components/Modals/UsuarioModal.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="col-12">
+    <div class="p-fluid formgrid grid">
+      <div class="field col-12">
+        <label for="nome">Nome</label>
+        <InputText
+          v-model="usuario.nome"
+          id="nome"
+          type="text"
+          autofocus
+          required />
+      </div>
+      <div class="field col-12">
+        <label for="email">Email</label>
+        <InputText v-model="usuario.email" id="email" type="text" required />
+      </div>
+      <div class="field col-12">
+        <label for="senha">Senha</label>
+        <Password
+          v-model="usuario.senha"
+          id="senha"
+          :feedback="false"
+          toggleMask
+          required />
+      </div>
+      <div class="field col-12">
+        <label class="mb-3">Permisssões</label>
+        <div class="formgrid grid">
+          <div class="field-radiobutton col-6">
+            <RadioButton
+              id="admin"
+              name="type"
+              value="1"
+              v-model="usuario.type" />
+            <label for="admin">Administrador</label>
+          </div>
+          <div class="field-radiobutton col-6">
+            <RadioButton
+              id="comum"
+              name="type"
+              value="0"
+              v-model="usuario.type" />
+            <label for="comum">Comum</label>
+          </div>
+          <Button @click="show"></Button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    usuario: Object
+  },
+  methods: {
+    show() {
+      console.log(this.usuario)
+    }
+  }
+}
+</script>
+
+<style></style>
