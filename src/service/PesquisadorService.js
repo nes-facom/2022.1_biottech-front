7e6a88fefbb7602056439e05476dff07e70fdb7f
Marialuisabcs <maria.lbcs@icloud.com>
2022-06-17 15:02:28 -0400

    feat: removes id from headers

--- src/service/PesquisadorService.js
+++ src/service/PesquisadorService.js
@@ -1,30 +1,36 @@
-import AuthService from "./AuthService";
-import axios from "axios";
-import { API_ENDPOINT } from "../constants";
-
+import AuthService from './AuthService'
+import axios from 'axios'
+import { API_ENDPOINT } from '../constants'
 
 class Pesquisador {
   async #getPesquisador(search, page) {
-    return axios.get(`${API_ENDPOINT}/pesquisador/getPesquisadores.json?page=${page}&limit=10&active=true&search=${search}`, this.buildAuthHeader())
+    return axios.get(
+      `${API_ENDPOINT}/pesquisador/getPesquisadores.json?page=${page}&limit=10&active=true&search=${search}`,
+      this.buildAuthHeader()
+    )
   }
 
   async #getPesquisadorInactive(search, page) {
-    return axios.get(`${API_ENDPOINT}/pesquisador/getPesquisadores.json?page=${page}&limit=10&active=false&search=${search}`, this.buildAuthHeader())
+    return axios.get(
+      `${API_ENDPOINT}/pesquisador/getPesquisadores.json?page=${page}&limit=10&active=false&search=${search}`,
+      this.buildAuthHeader()
+    )
   }
 
   getPesquisadores(disable, page, search, onFetch) {
     if (disable) {
-      this.#getPesquisadorInactive(search, page)
-        .then(response => onFetch(response.data))
+      this.#getPesquisadorInactive(search, page).then((response) =>
+        onFetch(response.data)
+      )
     } else {
-      this.#getPesquisador(search, page)
-        .then(response => onFetch(response.data))
+      this.#getPesquisador(search, page).then((response) =>
+        onFetch(response.data)
+      )
     }
   }
 
   getPesquisadorHeaders() {
     const columns = [
-      { field: 'id', header: 'ID' },
       { field: 'nome', header: 'Nome' },
       { field: 'instituicao', header: 'Instituição' },
       { field: 'setor', header: 'Setor' },
@@ -36,15 +42,13 @@ class Pesquisador {
     return columns
   }
 
-
   buildAuthHeader() {
     return {
       headers: {
-        'Authorization': `Bearer ${AuthService.getJWTToken()}`
+        Authorization: `Bearer ${AuthService.getJWTToken()}`
       }
     }
   }
 }
 
-
-export default new Pesquisador();
+export default new Pesquisador()
