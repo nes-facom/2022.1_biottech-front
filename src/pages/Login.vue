d39833a95db40b2187d9294cdcaab83a53d25d75
LeonardoVileela <leovilela.empresa@gmail.com>
2022-07-02 03:05:20 -0400

    disabled list in configuration

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -60,6 +60,11 @@
               label="Entrar"
               class="p-button-info w-10 text-xl"></Button>
           </div>
+          <div class="grid justify-content-center mb-2">
+            <p style="margin-top: 20px">
+              <a id="installApp" style="cursor: pointer"></a>
+            </p>
+          </div>
         </template>
       </Card>
     </div>
@@ -80,6 +85,84 @@ export default {
   computed: {
     logoColor() {
       return 'white'
+    }
+  },
+  mounted() {
+    if (window.location.href.includes('/login')) {
+      const installApp = document.getElementById('installApp')
+
+      window.addEventListener('load', () => {
+        registerSW()
+      })
+
+      async function registerSW() {
+        if ('serviceWorker' in navigator) {
+          try {
+            await navigator.serviceWorker.register('sw.js')
+          } catch (e) {
+            console.log(`SW registration failed`)
+          }
+        }
+      }
+
+      var isChrome = Boolean(window.chrome)
+      var isIOSSafari = !!window.navigator.userAgent.match(
+        /Version\/[\d\.]+.*Safari/
+      )
+
+      if (isChrome) {
+        if (
+          window.matchMedia('(display-mode: standalone)').matches ||
+          window.navigator.standalone === true
+        ) {
+        } else {
+          installApp.innerText = 'Instalar App'
+          let deferredPrompt
+
+          window.addEventListener('beforeinstallprompt', (e) => {
+            e.preventDefault()
+            deferredPrompt = e
+            // Update UI notify the user they can add to home screen
+            installApp.style.display = 'block'
+          })
+
+          installApp.addEventListener('click', (e) => {
+            // hide our user interface that shows our A2HS button
+            installApp.style.display = 'none'
+            // Show the prompt
+
+            deferredPrompt.prompt()
+
+            // Wait for the user to respond to the prompt
+            deferredPrompt.userChoice.then((choiceResult) => {
+              if (choiceResult.outcome === 'accepted') {
+              } else {
+              }
+              deferredPrompt = null
+            })
+          })
+        }
+      } else if (isIOSSafari) {
+        if (
+          window.matchMedia('(display-mode: standalone)').matches ||
+          window.navigator.standalone === true
+        ) {
+        } else {
+          $(document).Toasts('create', {
+            class: 'bg-info',
+            title: 'ATENÇÃO',
+            position: 'bottomRight',
+            body: 'Instale este aplicativo no seu Iphone: Clique na guia <img style="width: 25px;" src="icon/icon-iphone.jpg" alt=""> e depois em "Adicionar à Tela Início".'
+          })
+        }
+      } else {
+        $(document).Toasts('create', {
+          class: 'bg-info',
+          title: 'ATENÇÃO',
+          position: 'bottomRight',
+          body: 'Para instalar o APP no Android, Windows ou Mac OS é necessário entrar nesse site usando um navegador Google Chrome, para instalar no Iphone, use o navegador Safari.'
+        })
+      }
     }
   },
   methods: {
